935ac9f92f3545cfeec49cd5ec5055ddfd36ea05
Hao Jin <haoj@andrew.cmu.edu>
2017-03-25 18:25:41 -0400

    modified function interfaces

--- dcube.py
+++ dcube.py
@@ -2,43 +2,43 @@ import psycopg2
 import sys
 import os
 
-class Table:
-    def __init__(self, cur, tb):
-        self.cur = cur
-        self.tb = tb
-    def __del__(self):
-        cur.execute("DROP TABLE %s;" % self.tb)
+global conn
+conn = None
 
 
-def copy_table(cur, src, cpy):
+def copy_table(src, cpy):
+    cur = conn.cursor()
     try:
         cur.execute("CREATE TABLE %s AS TABLE %d;" % (src, cpy))
-    except:
+    except psycopg2.Error:
         print "Error when copying %s to %s" % (src, cpy)
+    conn.commit()
+    cur.close()
 
 
-def drop_table(cur, tb):
+def drop_table(tb):
+    cur = conn.cursor()
     try:
         cur.execute("DROP TABLE %s;" % tb)
-    except:
+    except psycopg2.Error:
         print "Error when dropping %s" % tb
+    conn.commit()
+    cur.close()
 
 
-def get_distinct_val_table(cur, new_tb, tb, col):
+def get_distinct_val(new_tb, tb, col):
+    cur = conn.cursor()
     try:
         cur.execute("CREATE TABLE %s AS SELECT DISTINCT %s FROM %s;" % (new_tb, col, tb))
-    except:
+    except psycopg2.Error:
         print "Error in get_distinct_val on table %s col %s" % (tb, col)
+    conn.commit()
+    cur.close()
 
 os.system("/usr/lib/postgresql/9.2/bin/pg_ctl -D $HOME/826prj/ -o '-k /tmp' start")
-query = raw_input("press to continue...")
+query = raw_input("press to continue...\n")
 username = os.environ['USER']
 port = 15000
-conn = psycopg2.connect(dbname=username, user=username, password="", port=port)
-cur = conn.cursor()
-query = raw_input("input the query to execute...\n")
-cur.execute(query)
-rows = cur.fetchall()
-for row in rows:
-    print row[0]
+conn = psycopg2.connect(dbname=username, user=username, password="", port = port)
+copy_table("darpa", )
 os.system("/usr/lib/postgresql/9.2/bin/pg_ctl -D $HOME/826prj stop")
