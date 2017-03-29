d51a522c423b1f37011480636a439913790b3969
Hao Jin <haoj@andrew.cmu.edu>
2017-03-29 00:44:13 -0400

    added comments for dcube_params

--- dcube_params.py
+++ dcube_params.py
@@ -1,7 +1,11 @@
+# The port to use
 PGPORT = 15000
 
+# Flag to control bucket type, 1 for bucket by day, 0 for bucket by hour
 BUCKET_FLAG = 1
 
+# List of columns for DARPA TCP DUMP data
 columns = ['src', 'dest', 'bucket']
 
+# Flag for whether to binarize the count, 1 for binarize, 0 for not
 BINARY_FLAG = 1
